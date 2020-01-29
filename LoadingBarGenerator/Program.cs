11b223179f4812c0000343726ebac943a0417d24
Julien FONTANA <jfont@LAPTOP-I73V7O29>
2020-01-29 16:39:39 +0100

    Add project files.

---
+++ LoadingBarGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LoadingBarGenerator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var lbg = new LoadingBar();
+            lbg.Display();
+        }
+    }
+}
